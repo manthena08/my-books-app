33150e25f08b0dcd8ca7d32f3cf2b16435b217a2
Manthea <Varma.Manthea@peoplefluent.com>
2017-08-02 21:22:23 -0400

    Getting data for library, assign for different shelfs, search for user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookFinder from './BookFinder'
@@ -7,13 +7,20 @@ import BookLibrary from './BookLibrary'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
   }
-
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState( {
+        books: books
+      })
+    })   
+  }
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookLibrary />
+          <BookLibrary availableBooks={this.state.books}/>
         )} />
 
         <Route path="/search" render={() => (
--- src/BookFinder.js
+++ src/BookFinder.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import * as BooksAPI from './BooksAPI'
+import SingleBook from './SingleBook'
 class BookFinder extends Component {
-    render() {
-        return (
-            <div className="search-books">
-                <div className="search-books-bar">
-                    <Link className="close-search" to="/">Close</Link>
-                    <div className="search-books-input-wrapper">
-                        {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author" />
+	state = {
+		query: '',
+		availableBook: []
+	}
 
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    <ol className="books-grid"></ol>
-                </div>
-            </div>
-        )
-    }
+	updateQuery = (value) => {
+		this.setState({
+			query: value.trim()
+		})
+		BooksAPI.search(value, 20)
+			.then(data => {
+				this.setState({
+					availableBook: data
+				})
+			})
+
+	}
+	render() {
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link className="close-search" to="/">Close</Link>
+					<div className="search-books-input-wrapper">
+						<input type="text" placeholder="Search by title or author"
+							value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid">
+						{this.state.availableBook.map((book) => (
+							<SingleBook book={book} />
+						))}
+
+					</ol>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default BookFinder
