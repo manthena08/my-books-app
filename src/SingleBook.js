edec84aa9edd7578ba470f60941c1f1c7c7be369
Manthea <Varma.Manthea@peoplefluent.com>
2017-08-05 10:48:23 -0400

    refactor:  moving static styles from DOM into stylesheet

--- src/SingleBook.js
+++ src/SingleBook.js
@@ -13,7 +13,7 @@ class SingleBook extends Component {
 				<div className="book">
 					<div className="book-top">
 						<div className="book-cover"
-							style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+							style={{backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
 						<div className="book-shelf-changer">
 							<select value={book.shelf} onChange={(event) => this.props.updateShelf(book,event.target.value)}>
 								<option value="none" disabled>Move to...</option>
