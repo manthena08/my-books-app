a1fce23019c6f6da3f63b83c52fb308b1e2f8c3b
Manthea <Varma.Manthea@peoplefluent.com>
2017-08-06 10:44:21 -0400

    Check for imageLink exists or not

--- src/SingleBook.js
+++ src/SingleBook.js
@@ -13,9 +13,9 @@ class SingleBook extends Component {
 				<div className="book">
 					<div className="book-top">
 						<div className="book-cover"
-							style={{backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+							style={(book.imageLinks) ? { backgroundImage: `url(${book.imageLinks.thumbnail})` } : {}}></div>
 						<div className="book-shelf-changer">
-							<select value={book.shelf} onChange={(event) => this.props.updateShelf(book,event.target.value)}>
+							<select value={book.shelf} onChange={(event) => this.props.updateShelf(book, event.target.value)}>
 								<option value="none" disabled>Move to...</option>
 								<option value="currentlyReading">Currently Reading</option>
 								<option value="wantToRead">Want to Read</option>
